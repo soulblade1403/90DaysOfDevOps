cf981e577ee0039bfa1d90e10b4526f4701029a6
soulblade <vuphuoc91@gmail.com>
2023-06-07 21:42:11 +0700

    Fix some bugs when install docker and k8s plugin

--- 2022/Days/Kubernetes/scripts/common.sh
+++ 2022/Days/Kubernetes/scripts/common.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Variable Declaration
-KUBERNETES_VERSION="1.23.3-00"
+KUBERNETES_VERSION="1.26.5-00"
 
 # disable swap 
 sudo swapoff -a
@@ -42,50 +42,41 @@ EOF
 sudo sysctl --system
 
 #Clean Install Docker Engine on Ubuntu
-sudo apt-get remove docker docker-engine docker.io containerd runc
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
 sudo apt-get update -y
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release software-properties-common
 
 #Add Docker’s official GPG key:
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 #set up the stable repository
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 #Install Docker Engine
 sudo apt-get update -y
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 #Configure containerd
 sudo mkdir -p /etc/containerd
 containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
 
 #restart containerd
 sudo systemctl restart containerd
 
 echo "ContainerD Runtime Configured Successfully"
-
 #Installing kubeadm, kubelet and kubectl
-sudo apt-get update -y 
-sudo apt-get install -y apt-transport-https ca-certificates curl
-
-#Google Cloud public signing key
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+# #Google Cloud public signing key
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 
 #Add Kubernetes apt repository
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+# echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 #Update apt package index, install kubelet, kubeadm and kubectl, and pin their version:
 sudo apt-get update -y
-
-sudo apt-get install -y kubelet kubectl kubeadm
-
-sudo apt-mark hold kubelet kubeadm kubectl
-
+sudo apt-get install -y kubelet=${KUBERNETES_VERSION} kubectl=${KUBERNETES_VERSION} kubeadm=${KUBERNETES_VERSION}
+sudo apt-mark hold kubelet kubeadm kubectl 
+sudo systemctl restart kubelet
